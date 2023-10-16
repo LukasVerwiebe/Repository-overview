17f953a552433bb61baf988b80fae03ea4cd23d6
Lukas <63674539+LukasVerwiebe@users.noreply.github.com>
2023-10-16 22:05:29 +0200

    update

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
-const wrapper = document.querySelector('.wrapper');
-const loginLink = document.querySelector('.login-link');
-const registerLink = document.querySelector('.register-link');
-const btnPopup = document.querySelector('.btnLogin-popup');
-const iconClose = document.querySelector('.icon-close');
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // Anzeigen des Menüs beim scrollen
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+            $('.navbar-new').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+            $('.navbar-new').removeClass("sticky");
+        }
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        } else {
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
 
-registerLink.addEventListener('click', ()=> {
-    wrapper.classList.add('active');
-});
+    // Slid-up Script Funktion
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+    });
 
-loginLink.addEventListener('click', ()=> {
-    wrapper.classList.remove('active');
-});
-
-btnPopup.addEventListener('click', ()=> {
-    wrapper.classList.add('active-popup');
-});
-
-iconClose.addEventListener('click', ()=> {
-    wrapper.classList.remove('active-popup');
+    // Ein und Ausschalten des menu/navbar bei Veränderung der Bildschrimgröße
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
 });
